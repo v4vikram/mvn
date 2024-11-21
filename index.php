6b948994e7cc656ad01fc3f7600a0be34b9939a4
v4vikram <v4vikram.dev@gmail.com>
2024-11-21 13:20:42 +0530

    fabicon

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home | MVN</title>
+    <link rel="icon" type="image/png" href="assets/img/favicon.webp">
     <!-- swiper.css -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <!-- fonts -->
